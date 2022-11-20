01573e09a997a82c5951518414e3bc0915481bc9
takuma-wada <9th.takuma@gmail.com>
2022-11-20 16:07:23 +0900

    :tada: initial commit

---
+++ app.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-row justify="space-around">
+        <v-icon
+          size="large"
+          color="green-darken-2"
+        >
+          mdi-domain
+        </v-icon>
+
+        <v-icon
+          size="large"
+          color="blue-darken-2"
+        >
+          mdi-message-text
+        </v-icon>
+
+        <v-icon
+          size="large"
+          color="purple-darken-2"
+        >
+          mdi-dialpad
+        </v-icon>
+
+        <v-icon
+          size="large"
+          color="teal-darken-2"
+        >
+          mdi-email
+        </v-icon>
+
+        <v-icon
+          size="large"
+          color="blue-grey-darken-2"
+        >
+          mdi-call-split
+        </v-icon>
+
+        <v-icon
+          size="large"
+          color="orange-darken-2"
+        >
+          mdi-arrow-up-bold-box-outline
+        </v-icon>
+      </v-row>
+    </v-app-bar>
+
+    <v-navigation-drawer penent>
+      Navigation Drawer
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        Main Contents
+      </v-container>
+    </v-main>
+
+    <v-bottom-navigation>
+      Button Navigation
+    </v-bottom-navigation>
+
+    <v-footer>
+      Footer
+    </v-footer>
+
+  </v-app>
+</template>
---
+++ plugins/vueitfy.ts
@@ -0,0 +1,13 @@
+import { createVuetify } from "vuetify";
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        components,
+        directives,
+    });
+
+    nuxtApp.vueApp.use(vuetify);
+});
